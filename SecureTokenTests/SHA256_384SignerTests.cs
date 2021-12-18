c146367f2ac91b70aad841594811bb4a474e829a
Mohammad Tarem <mohammad.tarem@live.com>
2021-12-18 15:04:37 +0330

    Adds unit test for SHA256 and 384

--- SecureTokenTests/SHA256_384SignerTests.cs
+++ SecureTokenTests/SHA256_384SignerTests.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using Xunit;
+using SecureToken;
+using System.Linq;
+using SecureTokenTests.Helpers;
+
 namespace SecureTokenTests
 {
-    public class SHA256_384Signer
+    public class SHA256_384SignerTests
     {
-        public SHA256_384Signer()
+        public SHA256_384SignerTests()
         {
+
+        }
+
+        [Fact]
+        public void Sha256_Must_Generate_32_Bytes_Hash()
+        {
+            SHA256Signer signer = new SHA256Signer(SecureRandomBytes.Generate(32));
+            var message = SecureRandomBytes.Generate(32);
+            var hash = signer.Hash(message);
+
+            Assert.Equal(32, hash.Length);
+            Assert.False(hash.SequenceEqual(message));
+
+        }
+
+        [Fact]
+        public void Sha384_Must_Generate_48_Bytes_Hash()
+        {
+            SHA384Signer signer = new SHA384Signer(SecureRandomBytes.Generate(48));
+            var message = SecureRandomBytes.Generate(48);
+            var hash = signer.Hash(message);
+
+            Assert.Equal(48, hash.Length);
+            Assert.False(hash.SequenceEqual(message));
+
         }
     }
 }
