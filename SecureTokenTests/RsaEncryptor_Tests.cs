97c9a3cde6707959d488fda474a5a31684b5f56f
Mohammad Tarem <mohammad.tarem@live.com>
2022-02-12 15:01:58 +0330

    Adds RsaEncryptor tests

---
+++ SecureTokenTests/RsaEncryptor_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Xunit;
+using SecureToken;
+using System.Security.Cryptography;
+
+
+namespace SecureTokenTests
+{
+    public class RsaEncryptor_Tests
+    {
+        public RsaEncryptor_Tests()
+        {
+
+        }
+
+        [Fact]
+        public void RsaEncryptor_Must_Encrypt_Decrypt()
+        {
+
+            RSACryptoServiceProvider r = new RSACryptoServiceProvider();
+
+            RsaEncryptor rsa = new RsaEncryptor(r.ToXmlString(true));
+            var message = SecureRandomBytes.Generate(10);
+
+            var cipher = rsa.Encrypt(message);
+            var plain = rsa.Decrypt(cipher);
+
+            Assert.True(message.SequenceEqual(plain));
+
+            
+        }
+
+        [Fact]
+        public void RsaEncryptor_Must_Encrypt_Decrypt_With_Param_Constructor()
+        {
+
+            RSACryptoServiceProvider r = new RSACryptoServiceProvider();
+
+            RsaEncryptor rsa = new RsaEncryptor(r.ExportParameters(true));
+            var message = SecureRandomBytes.Generate(10);
+
+            var cipher = rsa.Encrypt(message);
+            var plain = rsa.Decrypt(cipher);
+
+            Assert.True(message.SequenceEqual(plain));
+
+
+        }
+    }
+}
