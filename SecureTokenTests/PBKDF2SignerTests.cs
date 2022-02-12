224f31d652335ae1c6ef93148cf4570655cd2bec
Mohammad Tarem <mohammad.tarem@live.com>
2022-02-12 14:54:08 +0330

    Adds PBDKF2 singer

---
+++ SecureTokenTests/PBKDF2SignerTests.cs
@@ -0,0 +1,40 @@
+﻿using SecureToken;
+using System.Linq;
+using Xunit;
+namespace SecureTokenTests
+{
+    public class PBKDF2SignerTests
+    {
+        public PBKDF2SignerTests()
+        {
+        }
+
+        [Fact]
+        public void PBKDF2_Must_Generate_128_Bytes_Hash()
+        {
+
+            var keys = SecureRandomBytes.Generate(64);
+            PBKDF2Signer signer = new PBKDF2Signer( keys, 128, 1000);
+
+            var message = SecureRandomBytes.Generate(32);
+            var hash = signer.Hash(message);
+
+            Assert.Equal(128, hash.Length);
+
+        }
+
+        [Fact]
+        public void PBKDF2_Must_Generate_Same_Hash()
+        {
+            var keys = SecureRandomBytes.Generate(64);
+            PBKDF2Signer signer = new PBKDF2Signer(keys, 128, 1000);
+
+            var message = SecureRandomBytes.Generate(32);
+            var hash1 = signer.Hash(message);
+            var hash2 = signer.Hash(message);
+
+            Assert.True(hash1.SequenceEqual(hash2));
+
+        }
+    }
+}
