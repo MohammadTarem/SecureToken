a904322e3689cd4580dccea5a5cabec74a5f942c
Mohammad Tarem <Mohammad.Tarem@live.com>
2022-11-05 14:46:30 +0300

    Adds extention method to register authentication to ISeerviceCollection

--- SecureToken/Authentication/SecureTokenAuthenticationExtensions.cs
+++ SecureToken/Authentication/SecureTokenAuthenticationExtensions.cs
@@ -2,10 +2,18 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 
+using SecureToken.Authentication;
+
 namespace SecureToken.Authentication
 {
     public static class SecureTokenAuthenticationExtensions
     {
+
+        public static void AddSecureTokenAuthentication(this IServiceCollection services, Action<SecureTokenOptions> config, string authenticationHeader = "Authorization")
+        {
+            services.AddAuthentication(SecureTokenDefaults.AuthenticationScheme)
+                    .AddSecureTokenAuthentication(config, authenticationHeader);
+        }
         public static AuthenticationBuilder AddSecureTokenAuthentication(this AuthenticationBuilder builder,Action<SecureTokenOptions> config, string authenticationHeader = "Authorization")
         {
             var sOptions = new SecureTokenOptions { };
